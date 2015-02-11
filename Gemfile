1960b3d2785880868013e238e6aa6a7d9576aecc
Syuji <commentonly.is@gmail.com>
2015-02-12 00:05:52 +0900

    最初のコミット

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem 'dotenv'
+gem 'net-ssh', :require => 'net/ssh'
+gem 'net-scp', :require => 'net/scp'
---
+++ autowebcamtrans.rb
@@ -0,0 +1,66 @@
+# coding: utf-8
+
+require 'bundler'
+Bundler.require
+
+require 'logger'
+require 'date'
+
+class AutoWebcamTrans
+  def initialize(logger = nil)
+    #loggerインスタンスが無ければ作る
+    @logger = logger || Logger.new(STDERR)
+
+    # .ENVファイルロード
+    Dotenv.load
+
+    @fswebcam = ENV["FSWEBCAMPATH"] || '/usr/bin/fswebcam'
+
+    @skipframes = ENV["SKIPFRAMES"] ? ENV["SKIPFRAMES"].to_i : 70
+
+    # 当スクリプトファイルの所在
+    @scriptdir = File.expand_path(File.dirname(__FILE__))
+  end
+
+  def exec(fileName)
+    if fileName == nil then
+      imgDir = File.join(@scriptdir, "img/")
+      # 現在時刻を元に一時ファイル名を作る
+      fileName = DateTime.now.strftime("%Y-%m-%d-%H_%M_%S.jpg")
+      fileName = File.join(imgDir, fileName)
+    end
+    
+    # 撮影
+    shoot(fileName)
+
+    # ファイル転送
+    scpTransfer(fileName)
+  end
+
+  def shoot(filename)
+    shootLog = `#{@fswebcam} -S #{@skipframes} #{filename}`
+
+    @logger.info(shootlog)
+  end
+
+  def scpTransfer(filename)
+    # ファイル存在確認
+    unless File.exist? filename then
+      @logger.error("File #{filename} not found.")
+      return
+    end
+
+    hostname = ENV["SCPHOST"]
+    username = ENV["SCPUSER"]
+    password = ENV["SCPPASS"]
+    port = ENV["SCPPORT"]
+    remotedir = ENV["SCPREMOTEDIR"]
+
+    remotepath = File.join(remotedir, File.basename(filename))
+    Net::SSH.start(hostname, username, :password => password, :port => port) do |ssh|
+      ssh.scp.upload! filename, remotepath
+    end
+
+    @logger.info("Upload #{filename} success.")
+  end
+end
